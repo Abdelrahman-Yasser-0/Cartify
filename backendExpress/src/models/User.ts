ae2aed837587058717cd374488a8307fa9d416b0
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-11-29 22:58:55 +0200

    add model and validation for user type

---
+++ backendExpress/src/models/User.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  phone: String,
+
+  // store only the hashed password
+  passwordHash: { type: String, required: true },
+
+  role: { type: String, enum: ["user", "admin"], default: "user" },
+
+  shippingAddress: {
+    country: String,
+    city: String,
+    streetAddress: String,
+    apartment: String,
+    zip: String,
+  },
+
+  additionalInformation: {
+    company: String,
+    notes: String,
+  },
+
+  communicationPrefrences: {
+    email: Boolean,
+    sms: Boolean,
+  },
+
+  cart: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: Number,
+    },
+  ],
+  purchased: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      quantity: Number,
+      status: String,
+    },
+  ],
+  wishingList: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+    },
+  ],
+});
+const User = mongoose.model("users", UserSchema);
+export default User;
---
+++ backendExpress/src/validation/userValidation.ts
@@ -0,0 +1,16 @@
+import Joi from "joi";
+
+export default Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+
+  password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+
+  repeat_password: Joi.ref("password"),
+
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+});
+//   .with("username", "birth_year")
+//   .with("password", "repeat_password");
