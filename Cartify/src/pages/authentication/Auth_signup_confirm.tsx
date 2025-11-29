b96775d1947fbeb5ba91fe459dc29fa255253a8e
Abdelrahman-Yasser-0 <abdelrahmany006@gmail.com>
2025-11-30 01:05:48 +0200

    solved some issues in sign up confirmation msg

--- Cartify/src/pages/authentication/Auth_signup_confirm.tsx
+++ Cartify/src/pages/authentication/Auth_signup_confirm.tsx
@@ -24,11 +24,8 @@ const Auth_signup_confirm = ({ stepper }: { stepper: number }) => {
       </ul>
       {/*--------------------------------Congrats Message-------------------------------- */}
       <div className="w-full h-full flex flex-col items-center pt-4 text-center text-xl">
-<<<<<<< HEAD
-        <FaCircleCheck className="text-accent text-5xl shrink-0 pb-3" />
-=======
         <FaCircleCheck className="text-accent text-5xl shrink-0 " />
->>>>>>> main
+
         <p>
           Your Account Have been created
           <span className="text-accent"> Successfully!</span>
