ccfd3d5696d5d41f95b431a0e72617f5bf023d4f
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-11-29 21:34:57 +0200

    update user validation and user model

--- Backend/src/models/userModel.ts
+++ Backend/src/models/userModel.ts
@@ -2,16 +2,68 @@ import uservalidationSchema from "../validation/userValidation.ts";
 import mongoose from "mongoose";
 import { zodSchema } from "@zodyac/zod-mongoose";
 import productValidationSchema from "../validation/productValidation.ts";
+import { uuidv4 } from "zod/v4";
 
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
+    name: String,
+    email: { type: String, unique: true },
+    phone: String,
+
+    // store only the hashed password
     passwordHash: { type: String, required: true },
+
     role: { type: String, enum: ["user", "admin"], default: "user" },
+
+    shpingAddress: {
+      country: String,
+      city: String,
+      streetAddress: String,
+      apartment: String,
+      zip: String,
+    },
+
+    additionalInformation: {
+      company: String,
+      notes: String,
+    },
+
+    communicationPrefrences: {
+      email: Boolean,
+      sms: Boolean,
+    },
+
+    cart: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+      },
+    ],
+    purchased: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+        status: String,
+      },
+    ],
+    wishingList: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      },
+    ],
   },
-  { timestamps: true }
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
+
+// Create a virtual "id" that maps to "_id"
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString(); // convert ObjectId to string
+});
+
+// const userSchema = zodSchema(uservalidationSchema);
 
 const UserModel = mongoose.model("User", userSchema);
 export default UserModel;
--- Backend/src/validation/userValidation.ts
+++ Backend/src/validation/userValidation.ts
@@ -1,15 +1,43 @@
 import { z } from "zod";
+import productValidationSchema from "./productValidation.ts";
+import { count } from "console";
+
+import { v4 as uuidv4 } from "uuid";
 
 const userValidationSchema = z.object({
+  // _id: z.union([z.string(), z.any()]).optional(),
+  // __v: z.number().optional(),
+
   name: z.string(),
   email: z.string().email().trim().toLowerCase(),
+  phone: z.string().regex(/^\+?[0-9\s\-]{7,20}$/, "Invalid phone number"),
   password: z
     .string()
     .regex(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
       "Password must be 8+ chars, contain uppercase, lowercase, number, and special char."
     ),
+  hashPassword: z.string().optional(),
   role: z.enum(["user", "admin"]).default("user"),
+  shpingAddress: z.object({
+    country: z.string(),
+    city: z.string(),
+    streetAddress: z.string(),
+    apartment: z.string().optional(),
+    zip: z.string().regex(/^\d{3,10}$/, "Invalid postal code"),
+  }),
+  additionalInformation: z.object({ company: z.string(), notes: z.string() }),
+  communicationPrefrences: z.object({ email: z.boolean(), sms: z.boolean() }),
+  cart: z
+    .array(
+      z.object({
+        productId: z.union([z.string(), z.any()]).optional(),
+        quantity: z.number(),
+      })
+    )
+    .optional(),
+  purchased: z.array(z.any()),
+  wishingList: z.array(z.string()).optional(),
 });
 
 export default userValidationSchema;
