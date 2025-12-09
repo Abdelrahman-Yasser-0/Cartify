1e2d5787f35c68c591337bc674661bcc5a6af0f7
Abdelrahman-Yasser-0 <abdelrahmany006@gmail.com>
2025-12-10 01:45:09 +0200

    added the api integration for login and signup

--- Cartify/src/pages/authentication/Auth_Signup_Admin.tsx
+++ Cartify/src/pages/authentication/Auth_Signup_Admin.tsx
@@ -2,34 +2,31 @@ import { useState } from "react";
 import main_img from "../../images/cartify_login_img.png";
 import { FiShoppingCart } from "react-icons/fi";
 import { FaRegEye } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaRegEyeSlash } from "react-icons/fa6";
+
+import { FaCheckCircle } from "react-icons/fa";
+import { IoMdCloseCircle } from "react-icons/io";
+
+import { RiAccountCircleFill } from "react-icons/ri";
 import { IoClose } from "react-icons/io5";
+import Auth_Signup_2 from "./Auth_Signup_2";
+import { IoArrowBackOutline } from "react-icons/io5";
+import Auth_signup_confirm from "./Auth_signup_confirm";
+import { user } from "../types";
 import { users } from "../usersData";
 
-/**
- * --------------------------------------------------------------------------
- * What happens when a usestate gets changed ???
- * --------------------------------------------------------------------------
- * 1-The pages related to this usestate gets updated
- *
- *  1.1-Every statment outside the return get reexcuted ex:All consts (validateEmail, isEmailError) are re-calculated with the new state.
- *      console.logs run again But the Use State elments dosnt get recalc it is outside of the scope.
- *
- *  1.2-The statments inside the Return also gets updated if and only if the statment is reltated to the use State directly or indirectly
- *      if there is a function use this usestate inside the componetn even if the element dosnt use the use state directly and use the function only it gets updated
- *      cuz it uses the funciton which is using the use state
- * --------------------------------------------------------------------------
- */
-
-const Auth_Login = () => {
+const Auth_Signup_Admin = () => {
   const [email, setEmail] = useState<string>("");
+  const [fullName, setfullName] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [pass_see, setPass_see] = useState<boolean>(false);
   const [emailTouched, setEmailTouched] = useState<boolean>(false); //just used to handel when the user interacted with the input or not if the user interacted it is setted to false for the rest of the run until the user refresh the website
   const [passwordTouched, setPasswordTouched] = useState<boolean>(false);
-  const navigate = useNavigate();
-  const [loginError, setLoginError] = useState<boolean>(false);
+  const [fullNameTouched, setfullNameTouched] = useState<boolean>(false);
+
+  const [stepper, setStepper] = useState<number>(1);
+
   const containWhiteSpace = (email: string): boolean => {
     const spaceRegex = /\s/;
     return spaceRegex.test(email);
@@ -45,7 +42,7 @@ const Auth_Login = () => {
   //   return usernameRegex.test(email);
   // };
   const validDomain = (email: string): boolean => {
-    const domainRegex = /@(gmail|yahoo|outlook|hotmail|example)\.com$/i;
+    const domainRegex = /@(gmail|yahoo|outlook|hotmail)\.com$/i;
     return domainRegex.test(email);
   };
   const validExtinsion = (email: string): boolean => {
@@ -86,10 +83,14 @@ const Auth_Login = () => {
     const containNumber = /\d/;
     return containNumber.test(password);
   };
-  // const containSpecialChar = (password: string): boolean => {
-  //   const containspeacialchar = /[@$!%*?&]/;
-  //   return containspeacialchar.test(password);
-  // };
+  const containSpecialChar = (password: string): boolean => {
+    const containspeacialchar = /[@$!%*?&]/;
+    return containspeacialchar.test(password);
+  };
+  const validateFullName = (fullName: string): boolean => {
+    const validFullName = /^[a-zA-Z\u00C0-\u00FF\s-]{2,30}$/;
+    return validFullName.test(fullName);
+  };
 
   const vaildatePassword = (password: string): boolean => {
     //the regex logic
@@ -100,49 +101,78 @@ const Auth_Login = () => {
       containLowerCase(password) &&
       containUpperCase(password) &&
       containNumber(password) &&
-      // containSpecialChar(password) &&
+      containSpecialChar(password) &&
       passLenGt8(password)
     );
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     //submition handelr
     e.preventDefault();
-    if (validateEmail(email) && vaildatePassword(password)) {
+    if (validateEmail(email) && vaildatePassword(password) && "btn-disabled") {
+      const requestBody = {
+        name: fullName,
+        email: email,
+        phone: "",
+        password: password,
+        role: "Admin",
+        shippingAddress: {
+          country: "",
+          city: "",
+          streetAddress: "",
+          apartment: "",
+          zip: "",
+        },
+        additionalInformation: {
+          company: "",
+          notes: "",
+        },
+        communicationPrefrences: {
+          email: true,
+          sms: false,
+        },
+      };
       //send data using api
-      try {
-        const response = await fetch("http://127.0.0.1:3000/user/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ email: email, password: password }),
-        });
-        const data = await response.json();
-
-        if (response.status == 200) {
-          //sucuss
-          setLoginError(false);
-          console.log("Login is Correct", data);
-          localStorage.setItem("token", data.token);
-          localStorage.setItem("user", JSON.stringify(data.user));
-          navigate("/");
-        } else if (response.status == 404) {
-          console.log("yes the user is not created");
-        } else if (response.status == 401) {
-          setLoginError(true);
-        }
-      } catch (error) {
-        console.log(error);
+      const response = await fetch("http://127.0.0.1:3000/user/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+      //catch the api response
+      const data = await response.json();
+      if (response.status == 200) {
+        console.log("Signup Successful", data);
+        setStepper(3);
+      } else {
+        console.log(data.message);
       }
+
+      //   const userData: user = {
+      //     fullname: fullName,
+      //     email: email,
+      //     password: password,
+      //     country: country,
+      //     city: city,
+      //     streetAddress: streetAddress,
+      //     apartment: apartment,
+      //     zip: zip,
+      //     phoneNumber: phoneNumber,
+      //     id: users.length + 1,
+      //   };
+      //   users.push(userData);
+      console.log("yes valied");
+    } else {
+      console.log("no not valied");
     }
   };
   // console.log("Email state :" + !validateEmail(email) && emailTouched);
   // console.log("email toched :" + emailTouched);
-  // console.log("email valid :" + validateEmail(email));
-  // console.log("space :" + !containWhiteSpace(email));
+  console.log("email valid :" + validateEmail(email));
+  console.log("space :" + !containWhiteSpace(email));
 
   return (
-    <div className="flex w-full min-h-screen justify-center items-center gap-24p pt-10">
+    <div className="flex w-full min-h-screen justify-center items-center gap-24 pt-10">
       {/*--------------------------------Left Img-------------------------------- */}
       <div className=" overflow-hidden max-w-md lg:block hidden  ">
         <img src={main_img} alt="img_Not_Found 404" className="w-full" />
@@ -150,22 +180,98 @@ const Auth_Login = () => {
       {/*--------------------------------Form-------------------------------- */}
       <form
         onSubmit={handleSubmit}
-        className="lg:basis-[30%] lg:max-w-sm w-[80%] p-5 border rounded-2xl flex flex-col gap-5"
+        className={`lg:basis-[30%] lg:max-w-sm w-[80%] p-5 border rounded-2xl flex flex-col gap-5 ${
+          stepper === 3 && "hidden"
+        }`}
       >
         {/*--------------------------------Header-------------------------------- */}
-        <Link to="/" className="flex gap-4 cursor-pointer">
+        <Link to="/" className={`flex gap-4 cursor-pointer `}>
           <FiShoppingCart className="text-3xl sm:text-4xl bg-teal-600 rounded-md p-2 text-white" />
           <h2 className="py-1.5">Cartify</h2>
         </Link>
-        <h1 className="font-bold text-lg">Welcome back</h1>
-        <p className="text-sm text-gray-500">
-          Log in to your Cartify account to continue
-        </p>
+        <div className={`flex flex-col gap-2 `}>
+          <h1 className="font-semibold text-lg">Create your account</h1>
+          <p className="text-sm text-gray-500">
+            Sign up for Cartify to get started
+          </p>
+        </div>
+        {/*--------------------------------Stepper-------------------------------- */}
+        <ul className="steps steps-horizontal">
+          <li className="step step-accent">Account</li>
+          <li
+            data-content="✓"
+            className={`step  ${stepper >= 2 && "step-accent"}`}
+          >
+            Done
+          </li>
+        </ul>
         {/*--------------------------------Input Fields-------------------------------- */}
-        <div className="flex flex-col gap-4">
-          {/*--------------------------------Email-------------------------------- */}
+        <div className={`flex flex-col gap-4 ${stepper != 1 && "hidden"}`}>
+          {/*--------------------------------Full Name-------------------------------- */}
           <div>
             {/*--------------------------------Input Label Name-------------------------------- */}
+            <div className="label">
+              <span
+                className={`label-text ${
+                  !validateFullName(fullName) &&
+                  fullNameTouched &&
+                  "text-red-700"
+                }`}
+              >
+                Full Name
+              </span>
+            </div>
+            {/*--------------------------------Input -------------------------------- */}
+            <label
+              className={`input input-bordered flex items-center gap-2 ${
+                !validateFullName(fullName) && fullNameTouched
+                  ? "input input-error"
+                  : "input"
+              }`}
+            >
+              <RiAccountCircleFill className="text-gray-600" />
+              <input
+                type="text"
+                className="grow"
+                name="name"
+                autoComplete="name"
+                placeholder="Enter Your Full Name"
+                onChange={(e) => {
+                  setfullName(e.target.value);
+                }}
+                onBlur={() => {
+                  setfullNameTouched(true);
+                }}
+              />
+            </label>
+            {/*--------------------------------Error List -------------------------------- */}
+            <div
+              className={`w-full ${
+                !validateFullName(fullName) && fullNameTouched ? "" : "hidden"
+              }  bg-gray-100 rounded-lg mt-3 p-3 `}
+            >
+              <ul className={`flex flex-col gap-2`}>
+                <li
+                  className={`flex gap-2  items-center ${
+                    validateEmail(fullName) && "hidden"
+                  }`}
+                >
+                  {!validateEmail(fullName) && (
+                    <IoClose className="text-red-600 text-[16px] shrink-0 self-start " />
+                  )}
+                  <div className="flex flex-col gap-1">
+                    <p className="text-xs ">Please enter a valid Full name</p>
+                    <p className="text-xs ">
+                      A valid name cannot contain a Number or a Special
+                      character(ex:@,#,$.... )
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/*--------------------------------Email-------------------------------- */}
+          <div>
             <div className="label">
               <span
                 className={`label-text ${
@@ -175,7 +281,6 @@ const Auth_Login = () => {
                 Email
               </span>
             </div>
-            {/*--------------------------------Input -------------------------------- */}
             <label
               className={`input input-bordered flex items-center gap-2 ${
                 !validateEmail(email) && emailTouched
@@ -195,9 +300,9 @@ const Auth_Login = () => {
               <input
                 type="email"
                 className="grow"
-                placeholder="Email"
                 name="email"
-                autoComplete="email"
+                autoComplete="username"
+                placeholder="Enter Your Email"
                 onChange={(e) => {
                   setEmail(e.target.value);
                 }}
@@ -206,7 +311,6 @@ const Auth_Login = () => {
                 }}
               />
             </label>
-            {/*--------------------------------Error List -------------------------------- */}
             <div
               className={`w-full ${
                 !validateEmail(email) && emailTouched ? "" : "hidden"
@@ -258,7 +362,7 @@ const Auth_Login = () => {
               </ul>
             </div>
           </div>
-
+          {/*--------------------------------Password-------------------------------- */}
           <div>
             <div className="label">
               <span
@@ -293,9 +397,9 @@ const Auth_Login = () => {
               <input
                 type={`${pass_see ? "text" : "password"}`}
                 className="grow min-w-0"
-                placeholder="Password"
                 name="password"
                 autoComplete="new-password"
+                placeholder="Enter Your Password"
                 onChange={(e) => setPassword(e.target.value)}
                 onBlur={() => {
                   setPasswordTouched(true);
@@ -366,7 +470,7 @@ const Auth_Login = () => {
                     Password must contain at one single Number
                   </p>
                 </li>
-                {/* <li
+                <li
                   className={`flex gap-2  items-center ${
                     containSpecialChar(password) && "hidden"
                   }`}
@@ -377,54 +481,50 @@ const Auth_Login = () => {
                   <p className="text-xs">
                     Password must contain at least one Special character
                   </p>
-                </li> */}
+                </li>
               </ul>
             </div>
           </div>
-          {/*--------------------------------Remeber me + Forgot Password-------------------------------- */}
-          <div className="flex w-full justify-between">
-            <div>
-              <label className="cursor-pointer label flex gap-4">
-                <input type="checkbox" className="checkbox checkbox-xs" />
-                <span className="label-text">Remember me</span>
-              </label>
-            </div>
-            <Link
-              to="/auth/forgot_pass"
-              className="link link-primary !no-underline !text-cyan-600 text-center p-1"
-            >
-              Forgot password?
-            </Link>
-          </div>
         </div>
-        <p
-          className={`text-red-600 flex w-full justify-center items-center gap-4 ${
-            !loginError && "hidden  "
-          }`}
-        >
-          <span>
-            <IoClose className="text-red-600 text-[16px] shrink-0" />
-          </span>
-          Incorrect email or password.
-        </p>
-        <button
-          className={`text-white btn bg-teal-600 w-full btn-sm `}
-          type="submit"
-        >
-          Log in
-        </button>
-        <div className="flex w-full justify-center gap-3">
-          <p>Don't have an account?</p>
+
+        {/*--------------------------------Create acc -------------------------------- */}
+        <div className={`flex w-full justify-center gap-3 `}>
+          <p>Already have an account?</p>
           <Link
-            to="/auth/signup"
-            className="link link-primary !no-underline !text-cyan-600"
+            to=""
+            className={`link link-primary !no-underline !text-cyan-600 
+             `}
           >
-            Create account
+            log in
           </Link>
         </div>
+        {/*--------------------------------Submit button-------------------------------- */}
+        <div className={`flex flex-1 min-w-0  gap-5 w-full`}>
+          <button
+            className="btn w-1/4  btn-sm flex-auto flex-nowrap flex "
+            type="button"
+            onClick={() => {
+              setStepper((prev) => (prev -= 2));
+            }}
+          >
+            <IoArrowBackOutline className="shrink-0" />
+            Back
+          </button>
+          <button
+            className={`btn bg-teal-600 w-3/4 btn-sm flex-auto ${
+              !(validateEmail(email) && vaildatePassword(password)) &&
+              "btn-disabled"
+            }`}
+            type="submit"
+            onClick={() => setStepper(3)}
+          >
+            Create Account
+          </button>
+        </div>
       </form>
+      <Auth_signup_confirm stepper={stepper} />
     </div>
   );
 };
 
-export default Auth_Login;
+export default Auth_Signup_Admin;
--- backendExpress/src/index.ts
+++ backendExpress/src/index.ts
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.ts";
 import productRouter from "./routes/product.ts";
 import cartRouter from "./routes/cart.ts";
 import { requireAuth } from "./middlewares/authentcation.ts";
+import cors from "cors";
 // import userRouter from "./routes/users.ts";
 
 connect("mongodb://127.0.0.1:27017/s9")
@@ -13,6 +14,8 @@ connect("mongodb://127.0.0.1:27017/s9")
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
+
+app.use(cors());
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
