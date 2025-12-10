7e1414621fc363e45892a9bb7f162ae08f4cc8d1
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-12-11 01:17:01 +0200

    get statistics for admins API

--- backendExpress/src/routes/admin.ts
+++ backendExpress/src/routes/admin.ts
@@ -51,4 +51,96 @@ adminRouter.get("/userEmail/:userEmail", async (req, res) => {
   res.send(user);
 });
 
+//products statistics
+adminRouter.get(
+  "/product/stats",
+  // requireAuth,
+  // requireRole("admin"),
+  async (req, res) => {
+    try {
+      const stats = await Product.aggregate([
+        {
+          $facet: {
+            totalProducts: [{ $count: "count" }],
+
+            stockStats: [
+              {
+                $group: {
+                  _id: null,
+                  totalQuantity: { $sum: "$quantity" },
+                  totalSoldQuantity: { $sum: "$soldQuantity" },
+                  inStockProducts: {
+                    $sum: { $cond: ["$inStock", 1, 0] },
+                  },
+                  outOfStockProducts: {
+                    $sum: { $cond: ["$inStock", 0, 1] },
+                  },
+                },
+              },
+            ],
+
+            discountStats: [
+              {
+                $group: {
+                  _id: null,
+                  productsWithDiscount: {
+                    $sum: { $cond: ["$hasDiscount", 1, 0] },
+                  },
+                  avgDiscountValue: { $avg: "$discount" },
+                },
+              },
+            ],
+
+            ratingStats: [
+              {
+                $group: {
+                  _id: null,
+                  avgRating: { $avg: { $toDouble: "$rate" } },
+                },
+              },
+            ],
+
+            categories: [
+              {
+                $group: {
+                  _id: "$category",
+                  count: { $sum: 1 },
+                },
+              },
+              { $sort: { count: -1 } },
+            ],
+
+            priceStats: [
+              {
+                $group: {
+                  _id: null,
+                  minPrice: { $min: "$price" },
+                  maxPrice: { $max: "$price" },
+                  avgPrice: { $avg: "$price" },
+                },
+              },
+            ],
+
+            lowStock: [
+              { $match: { quantity: { $lte: 5 } } },
+              {
+                $project: {
+                  title: 1,
+                  quantity: 1,
+                },
+              },
+            ],
+          },
+        },
+      ]);
+
+      res.json({ success: true, stats: stats[0] });
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "An error occurred";
+      return res.status(500).send({ message: errorMessage });
+    }
+  }
+);
+
 export default adminRouter;
