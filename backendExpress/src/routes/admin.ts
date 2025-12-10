8d563e1013ecd091b7b6f59bbeb7cb108dc0db36
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-12-10 20:58:02 +0200

    create new product by admin

---
+++ backendExpress/src/routes/admin.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { Router } from "express";
+import express from "express";
+import Product from "../models/Product.ts";
+import { productValidation } from "../validation/productValidation.ts";
+// import { userValidation } from "../validation/userValidation.ts";
+import { requireRole } from "../middlewares/authorization.ts";
+const adminRouter = express.Router();
+
+//create product
+//only admins
+adminRouter.post("/product", async (req, res) => {
+  try {
+    await productValidation.validateAsync(req.body);
+
+    const createdProduct = await Product.create(req.body);
+    res.status(201).send({ data: { createdProduct } });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An error occurred";
+    res.status(400).send({ message: errorMessage });
+  }
+});
+
+//remove Products
+
+export default adminRouter;
