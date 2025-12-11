f6ecd89bd40fee3c6aac15056229ae4a8fec746f
Mohamed-Elahmar <mohamed1elahmar@gmail.com>
2025-12-11 16:50:33 +0200

    connection

--- backendExpress/src/index.ts
+++ backendExpress/src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { connect } from "mongoose";
 import userRouter from "./routes/user.ts";
@@ -9,10 +11,13 @@ import adminRouter from "./routes/admin.ts";
 import { requireRole } from "./middlewares/authorization.ts";
 import seedProducts from "./seeders/productsSeeder.ts";
 import seedUsers from "./seeders/userAndAdminSeeder.ts";
+import purchasedRouter from "./routes/purchased.ts";
 
-connect("mongodb://127.0.0.1:27017/cartifyDepiDB")
+const uri = process.env.ATLAS_URI ?? "mongodb://127.0.0.1:27017/cartifyDepiDB";
+// connect("mongodb://127.0.0.1:27017/cartifyDepiDB")
+connect(uri)
   .then(async () => {
-    console.log("MongoDB connected");
+    console.log("MongoDB connected", uri);
 
     // Run seeder
     await seedProducts();
@@ -28,6 +33,7 @@ app.use("/user", userRouter);
 app.use("/product", productRouter);
 app.use("/cart", requireAuth, cartRouter);
 app.use("/wishingList", requireAuth, wishingListRouter);
+app.use("/purchased", requireAuth, purchasedRouter);
 app.use("/admin", requireAuth, requireRole("admin"), adminRouter);
 
 app.get("/", (req, res) => {
